64b1bac7914cb8661dd54b427bec8db85f1e46fc
Dan Shearmur <dan.shearmur@semantico.com>
2013-05-10 20:05:05 +0100

    scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('habitsApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });

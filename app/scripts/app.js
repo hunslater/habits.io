df8403ffca2a4ab91fd403799e37c775419fa5fd
Dan Shearmur <dan.shearmur@semantico.com>
2013-05-10 20:52:54 +0100

    App flow and routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,6 +7,22 @@ angular.module('habitsApp', [])
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/habit', {
+        templateUrl: 'views/habits.html',
+        controller: 'HabitCtrl'
+      })
+      .when('/habit/:id', {
+        templateUrl: 'views/habits.html',
+        controller: 'HabitCtrl'
+      })
+      .when('/dashboard', {
+        templateUrl: 'views/dashboard.html',
+        controller: 'DashboardCtrl'
+      })
+      .when('/overview', {
+        templateUrl: 'views/overview.html',
+        controller: 'OverviewCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
